52884be86c1348dbffd6daa2fa777b4a3d7e786a
ManuelF <manuel.floriani1999@gmail.com>
2023-05-18 14:46:41 +0200

    [BONUS] Inserire attraverso i Vue-data una img

--- main.js
+++ main.js
@@ -9,7 +9,15 @@ const { createApp } = Vue;   //LA V DI VUE MAIUSCOLA!!
 createApp({
     data(){
         return {
-            message: 'HELLO WORLD!!'
+            message: 'HELLO WORLD!!',
+            image:"https://images.unsplash.com/photo-1683807179527-8e586297cb55?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1548&q=80"
         }
     }
-}).mount('#title')
+}).mount('#app')
+
+// export default{
+//     data(){
+//         return{
+//         }
+//     }
+// }
